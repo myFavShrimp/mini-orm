a81d946381897952c1052c6b7bfe78d73e77c610
myFavShrimp <mysavshrimp@tuta.io>
2023-04-09 22:51:31 +0200

    Added function for many to many query generation

--- mini-orm/src/query.rs
+++ mini-orm/src/query.rs
@@ -1,11 +1,11 @@
-use sea_query::{Expr, Query, SelectStatement};
+use sea_query::{Alias, Expr, JoinType, Query, SelectStatement};
 
 use crate::{
     entity::{Identifiable, TableEntity},
-    relation::OneToXRelation,
+    relation::{ManyToManyRelation, OneToXRelation},
 };
 
-pub async fn query<E>() -> SelectStatement
+pub fn query<E>() -> SelectStatement
 where
     E: TableEntity,
 {
@@ -15,7 +15,7 @@ where
         .to_owned()
 }
 
-pub async fn query_by_ids<E>(ids: Vec<<E as Identifiable>::IdType>) -> SelectStatement
+pub fn query_by_ids<E>(ids: Vec<<E as Identifiable>::IdType>) -> SelectStatement
 where
     E: Identifiable,
     sea_query::Value: From<<E as Identifiable>::IdType>,
@@ -27,7 +27,7 @@ where
         .to_owned()
 }
 
-pub async fn query_relational_by_id<A, B>(id: <A as Identifiable>::IdType) -> SelectStatement
+pub fn query_related_by_id<A, B>(id: <A as Identifiable>::IdType) -> SelectStatement
 where
     A: Identifiable + OneToXRelation<B>,
     B: Identifiable,
@@ -37,5 +37,31 @@ where
         .columns(<B as TableEntity>::all_columns())
         .from(<B as TableEntity>::table())
         .and_where(Expr::col(<A as OneToXRelation<B>>::target_relation_id_column()).eq(id))
+        .to_owned()
+}
+
+pub fn query_related_associative_by_id<A, B, R>(id: <A as Identifiable>::IdType) -> SelectStatement
+where
+    A: Identifiable + ManyToManyRelation<B, R>,
+    B: Identifiable + ManyToManyRelation<A, R>,
+    R: TableEntity,
+    sea_query::Value: From<<A as Identifiable>::IdType>,
+{
+    Query::select()
+        .columns(<B as TableEntity>::all_columns())
+        .from(<B as TableEntity>::table())
+        .join(
+            JoinType::Join,
+            <R as TableEntity>::table(),
+            Expr::col((
+                <R as TableEntity>::table(),
+                <B as ManyToManyRelation<A, R>>::own_relation_id_column(),
+            ))
+            .equals((
+                <B as TableEntity>::table(),
+                <B as Identifiable>::id_column(),
+            )),
+        )
+        .and_where(Expr::col(<A as ManyToManyRelation<B, R>>::own_relation_id_column()).eq(id))
         .to_owned()
 }
