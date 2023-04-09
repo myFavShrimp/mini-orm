f1a96ace11a0c2a92be9f47d6cca76b054aa9f54
myFavShrimp <mysavshrimp@tuta.io>
2023-04-09 13:17:19 +0200

    Added basic entity traits

--- mini-orm/src/lib.rs
+++ mini-orm/src/lib.rs
@@ -1,1 +1,14 @@
 
+pub trait Identifiable<I>: TableEntity {
+    fn id(&self) -> I;
+
+    fn id_column() -> Self::Iden;
+}
+
+pub trait TableEntity {
+    type Iden;
+
+    fn all_columns() -> Vec<Self::Iden>;
+
+    fn table() -> Self::Iden;
+}
